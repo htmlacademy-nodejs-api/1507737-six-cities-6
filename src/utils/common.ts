36837004a304ca4128ebef26a2ea7110a3c96deb
Viktor Zdorovtsev <v.zdvacew@gmail.com>
2024-01-17 20:57:47 +0700

    simplified `toBoolean` function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,19 +1,3 @@
-export function toBoolean(value: unknown): boolean {
-  if (typeof value === 'boolean') {
-    return value;
-  }
-  if (typeof value === 'number') {
-    return value !== 0;
-  }
-
-  if (typeof value === 'string') {
-    const lowercasedValue = value.trim().toLowerCase();
-    return lowercasedValue === 'true' || lowercasedValue === '1';
-  }
-
-  if (value instanceof Date) {
-    return !Number.isNaN(value.getTime());
-  }
-
-  return false;
+export function toBoolean(value: boolean | string | undefined) {
+  return value ? value !== 'false' : false;
 }
